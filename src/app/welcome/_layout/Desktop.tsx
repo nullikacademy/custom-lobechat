d8430fccd1b4a58d8898bcf150fb9af1f479c5e7
nullikacademy <167170431+nullikacademy@users.noreply.github.com>
2024-04-17 15:21:08 +0330

    Update Desktop.tsx

--- src/app/welcome/_layout/Desktop.tsx
+++ src/app/welcome/_layout/Desktop.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import { Logo } from '@lobehub/ui2';
+import { Logo } from '@lobehub/ui';
 import { PropsWithChildren, memo } from 'react';
 import { Center, Flexbox } from 'react-layout-kit';
 
