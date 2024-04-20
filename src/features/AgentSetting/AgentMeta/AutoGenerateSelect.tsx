505e9087848d5ef82324896e87fbb56ac1c8b652
nullikacademy <167170431+nullikacademy@users.noreply.github.com>
2024-04-20 22:02:10 +0330

    Update AutoGenerateSelect.tsx

--- src/features/AgentSetting/AgentMeta/AutoGenerateSelect.tsx
+++ src/features/AgentSetting/AgentMeta/AutoGenerateSelect.tsx
@@ -1,4 +1,4 @@
-import { ActionIcon } from '@lobehub/ui';
+import { ActionIcon } from 'nullikaiui';
 import { Select, SelectProps } from 'antd';
 import { useTheme } from 'antd-style';
 import { isString } from 'lodash-es';
