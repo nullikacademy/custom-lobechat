9d5d963f748e21d2318597978241c7a31aab1e82
nullikacademy <167170431+nullikacademy@users.noreply.github.com>
2024-04-20 21:58:38 +0330

    Update global.d.ts

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,4 +1,4 @@
-import type { LobeCustomStylish, LobeCustomToken } from '@lobehub/ui';
+import type { LobeCustomStylish, LobeCustomToken } from 'nullikaiui';
 import 'antd-style';
 import { AntdToken } from 'antd-style/lib/types/theme';
 
