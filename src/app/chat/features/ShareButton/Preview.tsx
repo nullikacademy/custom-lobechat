888fcf8bc544276c4c7843a54917efde0bb42210
nullikacademy <nullikacademy@gmail.com>
2024-05-08 17:03:52 +0330

    logo in share privew

--- src/app/chat/features/ShareButton/Preview.tsx
+++ src/app/chat/features/ShareButton/Preview.tsx
@@ -58,7 +58,7 @@ const Preview = memo<FieldType & { title?: string }>(
             <ChatList />
             {withFooter ? (
               <Flexbox align={'center'} className={styles.footer} gap={4}>
-                <Logo extra={'chat'} type={'combine'} />
+                <Logo/>
                 <div className={styles.url}>{pkg.homepage}</div>
               </Flexbox>
             ) : (
