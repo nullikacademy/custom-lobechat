038eba9401531c2c50401ba4b30f092fc689c106
nullikacademy <nullikacademy@gmail.com>
2024-05-09 12:11:12 +0330

    Update Header.tsx

--- src/app/market/_layout/Mobile/Header.tsx
+++ src/app/market/_layout/Mobile/Header.tsx
@@ -5,13 +5,11 @@ import { memo } from 'react';
 
 import { mobileHeaderSticky } from '@/styles/mobileHeader';
 
-import ShareAgentButton from '../../features/ShareAgentButton';
 
 const Header = memo(() => {
   return (
     <MobileNavBar
       center={<Logo type={'text'} />}
-      right={<ShareAgentButton mobile />}
       style={mobileHeaderSticky}
     />
   );
